32a7f104e55d99431d0c98e64dd38ba2a147fd16
Your Name <dharmaaprakash@gmail.com>
2025-11-02 13:23:26 +0530

    go

--- one.py
+++ one.py
@@ -1,7 +1,7 @@
 import os
 import flames1 as f
-#boy=str(input("Enter the boy name:"))
-#girl=str(input("Enter the girl name:"))
-boy = os.getenv("Boy")
-girl = os.getenv("Girl")
+boy=str(input("Enter the boy name:"))
+girl=str(input("Enter the girl name:"))
+#boy = os.getenv("Boy")
+#girl = os.getenv("Girl")
 print(f.flames1(Boy=boy, Girl=girl))
